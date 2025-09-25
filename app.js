22af54847b38852fa251c044006f4a7d2c2238a5
dalia <daliabalmus@gmail.com>
2025-09-25 23:39:19 +0300

    fix config

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
 	next();
 })
 
+app.get('/', (req, res) => {
+	res.send('hello world')
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
